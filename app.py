abcfb75d097733ee2f741a07699ceb0f2b97564e
fianngr <alfiannugroho95@gmail.com>
2023-12-22 16:21:16 +0700

    change file

--- app.py
+++ app.py
@@ -186,4 +186,4 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == "__main__":
-    app.run( host="0.0.0.0", port=int(os.environ.get("PORT",8080)))
+    app.run(debug=True, host="0.0.0.0",port=int(os.environ.get("PORT",8080)))
